aa22aa84d1a5d267ae005dc66c25a9a2d748ce8d
JoacoGE <joaquin.garaye@gmail.com>
2024-10-07 12:29:34 -0500

    Update c++.cpp

---
+++ c++.cpp
@@ -0,0 +1,15 @@
+/**
+ * @brief Punto de entrada principal para la aplicación.
+ * 
+ * Esta función es el punto de inicio del programa. Actualmente, no realiza ninguna operación.
+ * 
+ * @return int Código de estado de salida del programa.
+ */
+int main() {
+
+
+
+
+
+    return 0;
+}
